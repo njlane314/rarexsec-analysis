947da0b248636ba9830b5d9b77f7ed94ce3ebd8d
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-06-01 04:28:02 -0500

    Correcting CMake config

--- .setup.sh
+++ .setup.sh
@@ -7,5 +7,5 @@ setup nlohmann_json v3_11_2
 setup tbb v2021_9_0 -q e20
 setup libtorch v1_13_1b -q e20:prof
 
-#export NLOHMANN_JSON_INCLUDE_DIR=/cvmfs/larsoft.opensciencegrid.org/products/nlohmann_json/v3_11_2/include/nlohmann
 export LD_LIBRARY_PATH=$(pwd)/build/framework:$LD_LIBRARY_PATH
+export ROOT_INCLUDE_PATH=$(pwd)/build/framework:$ROOT_INCLUDE_PATH
--- plot_analysis.cpp
+++ plot_analysis.cpp
@@ -19,23 +19,27 @@ int main() {
         };
         AnalysisFramework::DataManager data_manager(dm_params);
 
+        std::cout << "set analysis space" << std::endl;
         AnalysisFramework::AnalysisSpace analysis_space;
         analysis_space
             .defineVariable("muon_momentum", "selected_muon_momentum_range", "Muon Momentum [GeV]", 50, 0, 5)
             .defineRegion("numu_cc_sel", "NuMu CC Selection", "NUMU_CC", "QUALITY");
 
+        std::cout << "set analysis space" << std::endl;
         AnalysisFramework::SystematicsController systematics_controller(data_manager.getVariableManager());
         systematics_controller
             .addWeightSystematic("RPA")
             .addWeightSystematic("CCMEC");
-
+        
+        std::cout << "set analysis space" << std::endl;
         AnalysisFramework::AnalysisRunner::Parameters runner_params {
             .data_manager = data_manager,
             .analysis_space = analysis_space,
             .systematics_controller = systematics_controller
         };
         AnalysisFramework::AnalysisRunner runner(runner_params);
-
+        
+        std::cout << "set analysis space" << std::endl;
         auto results = runner.run();
 
         std::cout << "Analysis run completed successfully." << std::endl;
