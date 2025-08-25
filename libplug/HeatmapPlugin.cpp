3b22e2be54faa17217a031f3cca06a7e1039b2de
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-08-25 18:25:28 +0100

    Add heatmap plotting plugin and catalog support

---
+++ libplug/HeatmapPlugin.cpp
@@ -0,0 +1,11 @@
+#include "HeatmapPlugin.h"
+#include <nlohmann/json.hpp>
+
+extern "C" analysis::IAnalysisPlugin* createPlugin(const nlohmann::json& cfg) {
+    return new analysis::HeatmapPlugin(cfg);
+}
+
+extern "C" void setPluginContext(analysis::AnalysisDataLoader* loader) {
+    analysis::HeatmapPlugin::setLoader(loader);
+}
+
