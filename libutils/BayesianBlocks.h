16e6e644eafdba378294786fdd03bc2a637a1fa3
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-06 22:03:51 +0100

    Refactor BayesianBlocks blocks

--- libutils/BayesianBlocks.h
+++ libutils/BayesianBlocks.h
@@ -21,26 +21,28 @@ using pair = std::pair<double, double>;
 using clock = std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using us = std::chrono::microseconds;
-} // namespace bb
+}
 
-bb::array blocks(bb::data_array data, bb::weights_array weights, double p = 0.01, bool counter = false,
-                 bool benchmark = false);
+bb::array blocks(bb::data_array data, bb::weights_array weights, double p = 0.01, bool counter = false, bool benchmark = false);
 bb::array blocks(bb::data_array data, double p = 0.01, bool counter = false, bool benchmark = false);
 
-} // namespace BayesianBlocks
+}
 
 namespace BayesianBlocks {
 
-bb::array blocks(bb::data_array data, bb::weights_array weights, double p, bool counter, bool benchmark) {
-    auto start = bb::clock::now();
+void validateInput(const bb::data_array& data, const bb::weights_array& weights) {
     if (data.size() != weights.size())
         throw std::domain_error("data and weights vectors are of different sizes");
     if (data.empty())
         throw std::invalid_argument("empty arrays provided as input");
     if (std::find_if(weights.begin(), weights.end(), [](double v) { return v <= 0.0; }) != weights.end())
         throw std::domain_error("invalid weights found in input");
-    if (std::unique(data.begin(), data.end()) != data.end())
+    auto tmp = data;
+    if (std::unique(tmp.begin(), tmp.end()) != tmp.end())
         throw std::invalid_argument("duplicated values found in input");
+}
+
+bb::array prepareHistogramData(bb::data_array& data, bb::weights_array& weights) {
     const auto N = data.size();
     std::vector<bb::pair> hist;
     hist.reserve(N);
@@ -57,12 +59,11 @@ bb::array blocks(bb::data_array data, bb::weights_array weights, double p, bool 
         edges[i + 1] = (data[i] + data[i + 1]) / 2.0;
     edges[N] = data[N - 1];
     assert(std::unique(edges.begin(), edges.end()) == edges.end());
+    return edges;
+}
+
+void findChangePoints(size_t N, const bb::weights_array& weights, const bb::array& edges, double ncp_prior, bool counter, bb::array& last, bb::array& best) {
     auto cash = [](double Nk, double Tk) { return Nk * std::log(Nk / Tk); };
-    auto ncp_prior = std::log(73.53 * p * std::pow(N, -0.478)) - 4.0;
-    bb::array last(N);
-    bb::array best(N);
-    auto init_time = bb::duration_cast<bb::us>(bb::clock::now() - start).count();
-    start = bb::clock::now();
     for (size_t k = 0; k < N; ++k) {
         bb::array A(k + 1);
         for (size_t r = 0; r <= k; ++r) {
@@ -75,10 +76,9 @@ bb::array blocks(bb::data_array data, bb::weights_array weights, double p, bool 
         if (counter)
             std::cout << '\r' << k << '/' << N << std::flush;
     }
-    if (counter)
-        std::cout << std::endl;
-    auto loop_time = bb::duration_cast<bb::us>(bb::clock::now() - start).count();
-    start = bb::clock::now();
+}
+
+bb::array buildEdgeResult(size_t N, const bb::array& last, const bb::array& edges) {
     std::vector<size_t> cp;
     for (auto i = N; i != 0; i = last[i - 1])
         cp.push_back(i);
@@ -86,6 +86,25 @@ bb::array blocks(bb::data_array data, bb::weights_array weights, double p, bool 
     std::reverse(cp.begin(), cp.end());
     bb::array result(cp.size(), 0.0);
     std::transform(cp.begin(), cp.end(), result.begin(), [edges](size_t pos) { return edges[pos]; });
+    return result;
+}
+
+bb::array blocks(bb::data_array data, bb::weights_array weights, double p, bool counter, bool benchmark) {
+    auto start = bb::clock::now();
+    validateInput(data, weights);
+    const auto N = data.size();
+    auto edges = prepareHistogramData(data, weights);
+    auto ncp_prior = std::log(73.53 * p * std::pow(N, -0.478)) - 4.0;
+    bb::array last(N);
+    bb::array best(N);
+    auto init_time = bb::duration_cast<bb::us>(bb::clock::now() - start).count();
+    start = bb::clock::now();
+    findChangePoints(N, weights, edges, ncp_prior, counter, last, best);
+    if (counter)
+        std::cout << std::endl;
+    auto loop_time = bb::duration_cast<bb::us>(bb::clock::now() - start).count();
+    start = bb::clock::now();
+    auto result = buildEdgeResult(N, last, edges);
     auto end_time = bb::duration_cast<bb::us>(bb::clock::now() - start).count();
     if (benchmark) {
         std::cout << "init: ";
@@ -107,13 +126,13 @@ bb::array blocks(bb::data_array data, double p, bool counter, bool benchmark) {
         hist[i]++;
     bb::data_array x;
     bb::weights_array weights;
-    for (auto &i : hist) {
+    for (auto& i : hist) {
         x.push_back(i.first);
         weights.push_back(i.second);
     }
     return blocks(x, weights, p, counter, benchmark);
 }
 
-} // namespace BayesianBlocks
+}
 
 #endif
