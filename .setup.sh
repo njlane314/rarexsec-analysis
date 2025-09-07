dc34ebb5d2b7b15e5f006b6008c8fc2115dc70b9
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-09-07 12:47:34 -0500

    Update

--- .setup.sh
+++ .setup.sh
@@ -1,4 +1,7 @@
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone_mcc9.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+setup sam_web_client
+
 setup root v6_28_12 -q e20:p3915:prof
 setup cmake v3_20_0 -q Linux64bit+3.10-2.17
 setup nlohmann_json v3_11_2
@@ -8,3 +11,4 @@ setup eigen v3_4_0
 
 export LD_LIBRARY_PATH=$(pwd)/build/framework:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$(pwd)/build/framework:$ROOT_INCLUDE_PATH
+
