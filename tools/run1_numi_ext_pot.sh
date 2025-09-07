4c279c5e7a7f4f1ff2677eb4c7d0be656d222e86
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-09-07 14:26:18 -0500

    Update

--- tools/run1_numi_ext_pot.sh
+++ tools/run1_numi_ext_pot.sh
@@ -6,10 +6,13 @@ source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 setup python v2_7_15a
 setup sam_web_client
 
+export OPENSSL_FORCE_FIPS_MODE=0
+
 START="2015-10-01T00:00:00"
 END="2016-07-31T23:59:59"
 
 python2 "/exp/uboone/app/users/guzowski/slip_stacking/getDataInfo.py" \
+  -v2 \
   --format EXT:tor101 \
   --where "begin_time > '${START}' AND end_time < '${END}'" \
   "$@"
