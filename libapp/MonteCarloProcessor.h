e81cae06faa1a8c81f1bb37a4665e67bcb583f83
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-09 00:34:49 +0100

    Fix MonteCarloProcessor parallel segfault

--- libapp/MonteCarloProcessor.h
+++ libapp/MonteCarloProcessor.h
@@ -5,8 +5,8 @@
 #include "BinnedHistogram.h"
 #include "ISampleProcessor.h"
 #include <unordered_map>
-#include <mutex>
-#include <tbb/parallel_for_each.h>
+#include <vector>
+#include <ROOT/RDataFrame.hxx>
 
 namespace analysis {
 
@@ -31,33 +31,34 @@ class MonteCarloProcessor : public ISampleProcessor {
 
     void contributeTo(VariableResult &result) override {
         log::info("MonteCarloProcessor::contributeTo", "Contributing histograms from sample:", sample_key_.str());
-        std::mutex mtx;
-        tbb::parallel_for_each(
-            nominal_futures_.begin(), nominal_futures_.end(),
-            [&, this](auto &entry) {
-                auto &[stratum_key, future] = entry;
-                if (future.GetPtr()) {
-                    auto hist =
-                        BinnedHistogram::createFromTH1D(result.binning_, *future.GetPtr());
-                    ChannelKey channel_key{stratum_key.str()};
-                    std::lock_guard<std::mutex> lock(mtx);
-                    result.strat_hists_[channel_key] =
-                        result.strat_hists_[channel_key] + hist;
-                    result.total_mc_hist_ = result.total_mc_hist_ + hist;
-                }
-            });
 
-        tbb::parallel_for_each(
-            variation_futures_.begin(), variation_futures_.end(),
-            [&, this](auto &entry) {
-                auto &[var_key, future] = entry;
-                if (future.GetPtr()) {
-                    auto hist =
-                        BinnedHistogram::createFromTH1D(result.binning_, *future.GetPtr());
-                    std::lock_guard<std::mutex> lock(mtx);
-                    result.raw_detvar_hists_[sample_key_][var_key] = hist;
-                }
-            });
+        std::vector<ROOT::RDF::RResultPtr<TH1D>> nominal_vec;
+        nominal_vec.reserve(nominal_futures_.size());
+        for (auto &entry : nominal_futures_) {
+            nominal_vec.push_back(entry.second);
+        }
+        ROOT::RDF::RunGraphs(nominal_vec);
+        for (auto &[stratum_key, future] : nominal_futures_) {
+            if (future.GetPtr()) {
+                auto hist = BinnedHistogram::createFromTH1D(result.binning_, *future.GetPtr());
+                ChannelKey channel_key{stratum_key.str()};
+                result.strat_hists_[channel_key] = result.strat_hists_[channel_key] + hist;
+                result.total_mc_hist_ = result.total_mc_hist_ + hist;
+            }
+        }
+
+        std::vector<ROOT::RDF::RResultPtr<TH1D>> variation_vec;
+        variation_vec.reserve(variation_futures_.size());
+        for (auto &entry : variation_futures_) {
+            variation_vec.push_back(entry.second);
+        }
+        ROOT::RDF::RunGraphs(variation_vec);
+        for (auto &[var_key, future] : variation_futures_) {
+            if (future.GetPtr()) {
+                auto hist = BinnedHistogram::createFromTH1D(result.binning_, *future.GetPtr());
+                result.raw_detvar_hists_[sample_key_][var_key] = hist;
+            }
+        }
     }
 
   private:
