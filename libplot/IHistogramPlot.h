f4bd25bd404634d23b818bba0907f7aa410d230a
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-08 16:52:49 +0100

    Ensure histogram plots save in batch mode

--- libplot/IHistogramPlot.h
+++ libplot/IHistogramPlot.h
@@ -26,11 +26,12 @@ class IHistogramPlot {
     virtual ~IHistogramPlot() = default;
 
     void drawAndSave(const std::string &format = "png") {
+        gROOT->SetBatch(kTRUE);
+        gSystem->mkdir(output_directory_.c_str(), true);
         this->setGlobalStyle();
         TCanvas canvas(plot_name_.c_str(), plot_name_.c_str(), 800, 600);
         this->draw(canvas);
-        canvas.SaveAs(
-            (output_directory_ + "/" + plot_name_ + "." + format).c_str());
+        canvas.SaveAs((output_directory_ + "/" + plot_name_ + "." + format).c_str());
     }
 
     static std::string sanitise(std::string s) {
