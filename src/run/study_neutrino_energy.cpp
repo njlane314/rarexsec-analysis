1a874dd788bfa325828bb536757c67cdd6265bba
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-12 22:19:01 +0100

    Expand neutrino energy plot range

--- src/run/study_neutrino_energy.cpp
+++ src/run/study_neutrino_energy.cpp
@@ -7,8 +7,11 @@ int main() {
   auto study = Study("MC Neutrino Energy")
     .data("config/catalogs/samples.json")
     .region("MC_ONLY", where("bnbdata == 0 && extdata == 0"))
-    .var("neutrino_energy")
-    .plot(stack("neutrino_energy").in("MC_ONLY").signal("channel_definitions"));
+    .var(VarDef("neutrino_energy").bins(100, 0.0, 10.0))
+    .plot(stack("neutrino_energy")
+              .in("MC_ONLY")
+              .signal("channel_definitions")
+              .logY());
 
   study.run("/tmp/neutrino_energy.root");
   return 0;
