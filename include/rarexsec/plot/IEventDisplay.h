71e37fce5f6c55ad60721c340270010898ebd101
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-12 15:39:08 +0100

    Decouple display canvas from data resolution

--- include/rarexsec/plot/IEventDisplay.h
+++ include/rarexsec/plot/IEventDisplay.h
@@ -12,35 +12,40 @@
 namespace analysis {
 
 class IEventDisplay {
-  public:
-    IEventDisplay(std::string tag, std::string title, int image_size,
-                  std::string output_directory)
-        : tag_(std::move(tag)), title_(std::move(title)), image_size_(image_size),
-          output_directory_(std::move(output_directory)) {}
+public:
+  IEventDisplay(std::string tag, std::string title, int canvas_size,
+                std::string output_directory)
+      : tag_(std::move(tag)), title_(std::move(title)),
+        canvas_size_(canvas_size),
+        output_directory_(std::move(output_directory)) {}
 
-    virtual ~IEventDisplay() = default;
+  virtual ~IEventDisplay() = default;
 
-    void drawAndSave(const std::string &format = "png") {
-        gSystem->mkdir(output_directory_.c_str(), true);
+  void drawAndSave(const std::string &format = "png") {
+    gSystem->mkdir(output_directory_.c_str(), true);
 
-        log::info("EventDisplay", "Saving", tag_, "to", output_directory_);
+    log::info("EventDisplay", "Saving", tag_, "to", output_directory_);
 
-        TCanvas canvas(tag_.c_str(), title_.c_str(), image_size_, image_size_);
-        canvas.SetCanvasSize(image_size_, image_size_);
-        this->draw(canvas);
-        canvas.Update();
-        canvas.SaveAs((output_directory_ + "/" + tag_ + "." + format).c_str());
-    }
+    TCanvas canvas(tag_.c_str(), title_.c_str(), canvas_size_, canvas_size_);
+    canvas.SetCanvasSize(canvas_size_, canvas_size_);
+    canvas.SetBorderMode(0);
+    canvas.SetFrameBorderMode(0);
+    canvas.SetFrameLineColor(0);
+    canvas.SetFrameLineWidth(0);
+    this->draw(canvas);
+    canvas.Update();
+    canvas.SaveAs((output_directory_ + "/" + tag_ + "." + format).c_str());
+  }
 
-  protected:
-    virtual void draw(TCanvas &canvas) = 0;
+protected:
+  virtual void draw(TCanvas &canvas) = 0;
 
-    std::string tag_;
-    std::string title_;
-    int image_size_;
-    std::string output_directory_;
+  std::string tag_;
+  std::string title_;
+  int canvas_size_;
+  std::string output_directory_;
 };
 
-}
+} // namespace analysis
 
 #endif
